b6cdd39d2cb684e0cb932a054333926b201de24d
Jaroslav Tylich <84792374+JonMagor@users.noreply.github.com>
2021-05-27 17:09:28 +0200

    GUI Inpot v02.8

--- pycoffee-tasting-input.py
+++ pycoffee-tasting-input.py
@@ -13,23 +13,28 @@ cols = ["Date", "User", "Country", "Name", "Roaster", "Processing", "RoastLevel"
 # , "BrewingMethod", "BrewingRecipe"
 #, "Známka", "Acidita", "Zemitost", "Intenzita", "Sladkost", "Poznámka"
 
-username = 'TestGUI_2.5'
+username = 'TestGUI_2.8'
 
 # Initialize dataframe 
 df = pd.DataFrame(columns=cols)
 
 
 # ------ Menu Definition ------ #
-menu_def = [['&File', ['&Open', '&Save', 'E&xit', 'Properties']],
-            ['&Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],
+menu_def = [['&Account', ['&Open', '&Save', 'E&xit', 'Properties']],
+            ['&History', ['Paste', ['Special', 'Normal', ], 'Undo'], ],
             ['&Help', '&About...'], ]
 
-# ------ Input Definition ------ #
-# --- Beans text input aliases for easy formatting --- #
-beans_size = (30,1)       # Size of field in characters, First number is width, second number is high
-beans_clear = True        # For do_not_clear parameter, False means it will be cleared after ANY event
-beans_font = ('Any 15')   # First number is width, second number is high
-beans_align = 'center'    # For justification parameter, False means it will be cleared after ANY event
+# ------ GLobal definitions ------ #
+base_font=('Any 15')        
+base_align='center'                 # For justification parameter
+
+# --- Function for generating text inputs for Beans origin & enabling of easy formatting --- #
+def BeansOrigin(key_sp, def_text):
+    return [sg.Input(key=key_sp, default_text=def_text,
+    size=(30,1),              # Size of the text input field
+    font=('Any 15'),
+    justification='center')]  # Alignment of text in the input field
+
 # --- Spin input aliases for easy formatting --- #
 spininput_size = (29,1)
 # --- Slider input aliases for easy formatting --- #
@@ -38,21 +43,17 @@ slidertext_size = (7,1)      # Size of text above the slider
 slidercount_size = (4,1)     # Size of counters of current value on slider
 
 # Buttons
-buttons = [sg.Button(button_text='Jde se ochutnávat!', tooltip='Kliknutím přejdeš na známkování chuti', font=beans_font)]
+buttons = [sg.Button(button_text='Jde se ochutnávat!', tooltip='Kliknutím přejdeš na známkování chuti', font=base_font)]
 
 # ------ "Beans" Layout Definition ------ #
 layoutBeans = [
     # ---- Menu, for future use, momentarily just for show ---- #
      [sg.Menu(menu_def, tearoff=True)],
     # ---- Fancy frame and title ---- #
-    [sg.Frame('',[
-     [sg.Text('Zrno', font=beans_font)],
-    # ---- Beans origin details ---- #
-     [sg.Input(key='Country', default_text = "Země - Kenya, Brazil...", size=beans_size, font=beans_font, justification=beans_align, do_not_clear=beans_clear)],
-     [sg.Input(key='Name', default_text = "Jméno - Kiwami, Diamond...", size=beans_size, font=beans_font, justification=beans_align, do_not_clear=beans_clear)],
-     [sg.Input(key='Roaster', default_text = "Pražírna - Motmot, Father's...", size=beans_size, font=beans_font, justification=beans_align, do_not_clear=beans_clear)],
-     [sg.Input(key='Processing', default_text = "Zpracování - natural, washed...", size=beans_size, font=beans_font, justification=beans_align, do_not_clear=beans_clear)],
-     [sg.Input(key='Variety', default_text = "Odrůda - Heirloom, Tabi...", size=beans_size, font=beans_font, justification=beans_align, do_not_clear=beans_clear)],
+    [sg.Frame('',layout=[
+     [sg.Text('Zrno', font=base_font)],
+    # ---- Generated Beans origin details ---- #
+     [sg.Column(layout=[BeansOrigin(k,d) for k,d  in [('Country',"Země - Kenya, Brazil..."),('Name',"Jméno - Kiwami, Diamond..."),('Roaster',"Pražírna - Motmot, Father's..."),('Processing',"Zpracování - natural, washed..."),('Variety',"Odrůda - Heirloom, Tabi...")]])],
     # ---- Beans processing details ---- #
     # -- Spin box with roasting levels -- *
      [sg.Spin(
@@ -65,18 +66,17 @@ layoutBeans = [
             '      5 Tmavé pražení'],
         initial_value='      3 Střední pražení',                                  
         size=spininput_size,
-        font=beans_font)],
+        font=base_font)],
     # -- Slider with Arabica/Robusta ratio -- *
-     [sg.Text('0', key='_LEFT_', size=slidercount_size, font=beans_font),                     # Robusta counter
-       sg.Text('Robusta', size=slidertext_size, font=beans_font, justification=beans_align),  # Robusta name
-       sg.Text('Arabica', size=slidertext_size, font=beans_font, justification=beans_align),  # Arabica name
-       sg.Text('100', key='_RIGHT_', size=slidercount_size, font=beans_font)],                # Arabica counter
-     [sg.Slider(key='Type', range=(1, 100), orientation='h', disable_number_display=True, default_value=100, font=beans_font, enable_events=True)],
+     [sg.Text('0', key='_LEFT_', size=slidercount_size, font=base_font),                     # Robusta counter
+       sg.Text('Robusta', size=slidertext_size, font=base_font, justification=base_align),  # Robusta name
+       sg.Text('Arabica', size=slidertext_size, font=base_font, justification=base_align),  # Arabica name
+       sg.Text('100', key='_RIGHT_', size=slidercount_size, font=base_font)],                # Arabica counter
+     [sg.Slider(key='Type', range=(1, 100), orientation='h', disable_number_display=True, default_value=100, font=base_font, enable_events=True)],
     # ---- Fancy frame ends here ---- #
-     ], 
-    element_justification='center')],
+     ], element_justification='center')],
     # ---- Button to submit and go for the next page ---- #
-    [sg.Column([buttons], justification=beans_align)]]
+    [sg.Column([buttons], justification=base_align)]]
 
 # ------ "Recepy" Layout Definition ------ #
 layoutRecepy = [
@@ -86,11 +86,11 @@ layoutRecepy = [
 
 
 # Create the window
-windowBeans = sg.Window("PyCoffee", layoutBeans, margins=(5,5), finalize=True)
+windowBeans = sg.Window("PyCoffee", layoutBeans, margins=(5,5), no_titlebar=False, finalize=True)
 
-# Clearance od default texts in text input fields in Beans windows
+ # Binding focus events from thinker on our PySimpleGUI "Beans Origins" text inputs
+windowBeans['Country'].block_focus(block = True)        # This will block any Focus on the window opening; so the first click also clears default text
 windowBeans['Country'].bind('<FocusIn>', '+FOCUS IN+')
-windowBeans['Country'].block_focus(block = True)
 windowBeans['Name'].bind('<FocusIn>', '+FOCUS IN+')
 windowBeans['Roaster'].bind('<FocusIn>', '+FOCUS IN+')
 windowBeans['Processing'].bind('<FocusIn>', '+FOCUS IN+')
@@ -100,7 +100,7 @@ windowBeans['Variety'].bind('<FocusIn>', '+FOCUS IN+')
 while True:
     event, values = windowBeans.read()
     print(event,values)
-    # If Arabica/Robusta slider is used, modify values of Arabica/Robusta counters, respectively
+   # If Arabica/Robusta slider is used, modify values of Arabica/Robusta counters, respectively
     if event in ('Type'):
          windowBeans.Element('_LEFT_').Update(100-values['Type'])
          windowBeans.Element('_RIGHT_').Update(values['Type'])
@@ -115,8 +115,11 @@ while True:
         windowBeans['Processing'].update('')
     if event == 'Variety+FOCUS IN+' and values['Variety'] == "Odrůda - Heirloom, Tabi...":
         windowBeans['Variety'].update('')    
-    # End program if user closes window or presses the "Jde se ochutnávat!" button
+    # If User presses the "Jde se ochutnávat!" button, open next Windows
     if event in ("Jde se ochutnávat!", sg.WIN_CLOSED, 'Exit'):
+        break
+    # End program if User closes window
+    if event in (sg.WIN_CLOSED, 'Exit'):
         break
 
 windowBeans.close()
