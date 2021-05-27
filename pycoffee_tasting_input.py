573f92830ae83ca8e81dc0eca5778c26b6a1b87e
vaculiko <vaculikondrej96@gmail.com>
2021-05-27 18:49:01 +0200

    Improved slider, autodelete list comprehension

--- pycoffee_tasting_input.py
+++ pycoffee_tasting_input.py
@@ -1,6 +1,8 @@
 import random
 import os
 from tkinter.constants import CENTER
+from types import resolve_bases
+from warnings import resetwarnings
 import pandas as pd
 import PySimpleGUI as sg
 from datetime import date
@@ -8,16 +10,13 @@ from pathlib import Path
 
 # ------ Database Definition ------ #
 # Entry info: date, user
-# Been info: Country, Name, Roaster, Processing, Roast Level, Type, Variety, Brewing Method, Brewing Recipe
+# Bean info: Country, Name, Roaster, Processing, Roast Level, Type, Variety, Brewing Method, Brewing Recipe
 cols = ["Date", "User", "Country", "Name", "Roaster", "Processing", "RoastLevel", "Type", "Variety"]
 # , "BrewingMethod", "BrewingRecipe"
 #, "Známka", "Acidita", "Zemitost", "Intenzita", "Sladkost", "Poznámka"
+df = pd.DataFrame(columns=cols) # Initialize dataframe 
 
 username = 'TestGUI_2.8'
-
-# Initialize dataframe 
-df = pd.DataFrame(columns=cols)
-
 
 # ------ Menu Definition ------ #
 menu_def = [['&Account', ['&Open', '&Save', 'E&xit', 'Properties']],
@@ -25,19 +24,21 @@ menu_def = [['&Account', ['&Open', '&Save', 'E&xit', 'Properties']],
             ['&Help', '&About...'], ]
 
 # ------ GLobal definitions ------ #
+sg.theme('DarkAmber')
 base_font=('Any 15')        
-base_align='center'                 # For justification parameter
+base_align='center'   # For justification parameter
 
 # --- Function for generating text inputs for Beans origin & enabling of easy formatting --- #
 def BeansOrigin(key_sp, def_text):
-    return [sg.Input(key=key_sp, default_text=def_text,
-    size=(30,1),              # Size of the text input field
-    font=('Any 15'),
-    justification='center')]  # Alignment of text in the input field
+    return [sg.Input(key=key_sp, 
+                     default_text=def_text,
+                     size=(30,1),              # Size of the text input field
+                     font=('Any 15'),
+                     justification='center')]  # Alignment of text in the input field
 
-# --- Spin input aliases for easy formatting --- #
+# --- Spin input --- #
 spininput_size = (29,1)
-# --- Slider input aliases for easy formatting --- #
+# --- Slider input --- #
 sliderinput_size = (38,20)   # Size of the actual slider in characters, first number is width, second number is high
 slidertext_size = (7,1)      # Size of text above the slider
 slidercount_size = (4,1)     # Size of counters of current value on slider
@@ -69,16 +70,16 @@ layoutBeans = [
         font=base_font)],
     # -- Slider with Arabica/Robusta ratio -- *
      [sg.Text('0', key='_LEFT_', size=slidercount_size, font=base_font),                     # Robusta counter
-       sg.Text('Robusta', size=slidertext_size, font=base_font, justification=base_align),  # Robusta name
-       sg.Text('Arabica', size=slidertext_size, font=base_font, justification=base_align),  # Arabica name
+       sg.Text('Robusta', size=slidertext_size, font=base_font, justification=base_align),   # Robusta name
+       sg.Text('Arabica', size=slidertext_size, font=base_font, justification=base_align),   # Arabica name
        sg.Text('100', key='_RIGHT_', size=slidercount_size, font=base_font)],                # Arabica counter
-     [sg.Slider(key='Type', range=(1, 100), orientation='h', disable_number_display=True, default_value=100, font=base_font, enable_events=True)],
+    [sg.Slider(key='Type', range=(0, 100), resolution=5, orientation='h', disable_number_display=True, default_value=100, font=base_font, enable_events=True)],
     # ---- Fancy frame ends here ---- #
      ], element_justification='center')],
     # ---- Button to submit and go for the next page ---- #
     [sg.Column([buttons], justification=base_align)]]
 
-# ------ "Recepy" Layout Definition ------ #
+# ------ "Recepty" Layout Definition ------ #
 layoutRecepy = [
     [sg.Input(default_text = "Způsob přípravy - espresso, V60...", key='BrewingMethod', size=(30,20))],
             [sg.Input(default_text = "Recept příprav - inverted aeropress, ristretto...", key='BrewingRecipe', size=(30,20))],
@@ -90,20 +91,21 @@ windowBeans = sg.Window("PyCoffee", layoutBeans, margins=(5,5), no_titlebar=Fals
 
  # Binding focus events from thinker on our PySimpleGUI "Beans Origins" text inputs
 windowBeans['Country'].block_focus(block = True)        # This will block any Focus on the window opening; so the first click also clears default text
-windowBeans['Country'].bind('<FocusIn>', '+FOCUS IN+')
-windowBeans['Name'].bind('<FocusIn>', '+FOCUS IN+')
-windowBeans['Roaster'].bind('<FocusIn>', '+FOCUS IN+')
-windowBeans['Processing'].bind('<FocusIn>', '+FOCUS IN+')
-windowBeans['Variety'].bind('<FocusIn>', '+FOCUS IN+')
+autodelete = ['Country', 'Name', 'Roaster', 'Processing', 'Variety']
+[windowBeans[key].bind('<FocusIn>', '+FOCUS IN+') for key in autodelete] # List comprehension for space reduction
+# windowBeans['Country'].bind('<FocusIn>', '+FOCUS IN+')    # can be deleted
+# windowBeans['Name'].bind('<FocusIn>', '+FOCUS IN+')       # can be deleted
+# windowBeans['Roaster'].bind('<FocusIn>', '+FOCUS IN+')    # can be deleted
+# windowBeans['Processing'].bind('<FocusIn>', '+FOCUS IN+') # can be deleted
+# windowBeans['Variety'].bind('<FocusIn>', '+FOCUS IN+')    # can be deleted
 
 # Create an event loop
 while True:
     event, values = windowBeans.read()
-    print(event,values)
    # If Arabica/Robusta slider is used, modify values of Arabica/Robusta counters, respectively
-    if event in ('Type'):
-         windowBeans.Element('_LEFT_').Update(100-values['Type'])
-         windowBeans.Element('_RIGHT_').Update(values['Type'])
+    if event == 'Type':
+         windowBeans.Element('_LEFT_').Update(100-int(values['Type']))
+         windowBeans.Element('_RIGHT_').Update(int(values['Type']))
     # Clear default text if the text input field is focused
     if event == 'Country+FOCUS IN+' and values['Country'] == "Země - Kenya, Brazil...":
         windowBeans['Country'].update('')
@@ -117,13 +119,13 @@ while True:
         windowBeans['Variety'].update('')    
     # If User presses the "Jde se ochutnávat!" button, open next Windows
     if event in ("Jde se ochutnávat!", sg.WIN_CLOSED, 'Exit'):
+        print(values)
         break
     # End program if User closes window
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
 
 windowBeans.close()
-print(values)
 
 # Console input for tasting info
 def input_tasting():
@@ -149,9 +151,6 @@ Path('data').mkdir(parents=True, exist_ok=True)
 file = 'data/pycoffee-'+ username +'.csv'
 hdr = False if os.path.isfile(file) else True
 df.to_csv(file, mode='a', index=False, header=hdr)
-
-
-
 print(df)
 
 # Přidat pamatování si předchozích textových inputů, když uživatel rozklikne, nabídne se mu, co psal dříve. Třeba 5 nejčastějších.
