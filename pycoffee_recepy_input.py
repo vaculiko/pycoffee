96b799e029adb590b29d340292b6576b893ae26e
Jaroslav Tylich <84792374+JonMagor@users.noreply.github.com>
2021-05-28 17:51:00 +0200

    Delete

--- pycoffee_recepy_input.py
+++
@@ -1,9 +0,0 @@
-import os
-from tkinter.constants import CENTER
-from types import resolve_bases
-from warnings import resetwarnings
-import pandas as pd
-import PySimpleGUI as sg
-from datetime import date
-from pathlib import Path
-
