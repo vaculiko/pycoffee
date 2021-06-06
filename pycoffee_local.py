9026eb2fbd4f774c267d29e5e19bae3160a744f8
vaculiko <vaculikondrej96@gmail.com>
2021-06-06 22:00:49 +0200

    Multiwindow with saving to csv

--- pycoffee_local.py
+++ pycoffee_local.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from pandas.core.frame import DataFrame
 from pycoffee_login import main as login
 from pycoffee_home import main as home
 from pycoffee_tasting_input import main as bean
@@ -7,7 +6,7 @@ from pycoffee_brewing_input import main as brew
 from pycoffee_rating_input import main as rating
 
 '''
-Window arrangement
+Window order
 1. login
 2. home
 3. tasting_input
@@ -17,21 +16,26 @@ Window arrangement
 
 TODO: Back button implementation
 '''
-
+# login screen
 user = login()
-print(user)
-
+# home screen with info
 select = home(user)
-print(select)
 
 if select == 'brew':
-    new_row = {}
-    new_row.update(bean(user))  # add entries to row dictionary
+    new_row = {} # empty dict to hold all tasting info
+    # Go through all windows and save output to dict
+    new_row.update(bean(user))  # .update() adds entries to row dictionary
     new_row.update(brew())
     new_row.update(rating())
-    print(new_row)
-    # df = DataFrame(new_row)
-    # df = df.append(new_row, ignore_index=True)
+    
+    # Load only database columns, no data
+    df = pd.read_csv(f'data/pycoffee-{user}.csv', header=0, index_col=False).head(0)
+    # add new_row to empty dataframe with correct column name and order
+    df = df.append(new_row, ignore_index=True)
+    # choose databse based on username
+    file = 'data/pycoffee-' + user + '.csv'
+    # append new_row of tasting to user database
+    df.to_csv(file, mode='a', index=False, header=False)
     print(df)
 
 if select == 'database':
