e93e13765db3a813b0d504b2857981a3a2ec4071
vaculiko <vaculikondrej96@gmail.com>
2021-06-06 22:15:07 +0200

    Random databse matches user generated structure

--- pycoffee_database.py
+++ pycoffee_database.py
@@ -4,8 +4,8 @@ from datetime import date
 from pathlib import Path
 
 # Define database columns
-cols = ["Datum", "Uživatel", "Země", "Region", "Zpracování", "Pražírna", "Příprava", "Recept", 
-        "Známka", "Acidita", "Zemitost", "Intenzita", "Sladkost", "Poznámka"]
+cols = ['Date','User','Country','Name','Roaster','Processing','Variety','RoastLevel','Type',
+        'BrewingMethod','BrewingRecipe','Rating','Sour','Sweet','Salty','Bitter','Note']
 # Initialize dataframe 
 df = pd.DataFrame(columns=cols)
 
@@ -27,12 +27,15 @@ def input_tasting():
 # Generate random data for visualisation testing
 user = ["Aestas", "Cruduk", "Talon", "Zorin"]
 country = ["Kenya", "Honduras", "Rwanda", "Ethipoia", "Costa Rica", "Brazil"]
-regions = ["Kiranga", "Cadexsa", "Kiyumba", "Dambi Uddo", "Blend", "Palmichal", None]
+name = ["Kiranga", "Cadexsa", "Kiyumba", "Dambi Uddo", "Blend", "Palmichal", None]
+roaster = ["Motmot", "Father's", "Rebelbean", "Laura Coffee", "Naughty dog", "Monogram"]
 process = ["Washed", "Natural", "Honey", "Carbon"]
-roastery = ["Motmot", "Father's", "Rebelbean", "Laura Coffee", "Naughty dog", "Monogram"]
+variety = ['Tabi', 'Bourbon', 'Catuai', 'Typica']
+roast_level = range(1,6) # stop value is exclusive
+type_ratio = [100, 90, 80]
 machine = ["Aeropress", "V60", "Espresso", "Frenchpress", "Moka"]
 recipe = ['aaa', 'bbb', 'ccc', None]
-data = [user, country, regions, process, roastery, machine, recipe]
+data = [user, country, name, roaster, process, variety, roast_level, type_ratio, machine, recipe]
 random.seed(42)
 
 def random_tasting():
@@ -40,12 +43,12 @@ def random_tasting():
     for i,col in enumerate(cols):
         if i == 0:
             row_dict[col] = str(date.today()) # date
-        if 1 <= i <= 7:
+        if 1 <= i <= 10:
             row_dict[col] = random.choice(data[i-1]) # random text input
-        if i > 7:
+        if i > 10:
             row_dict[col] = random.randint(1,5) # random numerical input
-        if i == 13:
-            row_dict[col] = None # no comment
+        if i == 16:
+            row_dict[col] = random.choice([None, 'S ledem', 'Mandle', 'Pomeranč', 'Karamelky'])
     return row_dict
 
 # Generate dataset with 50 random rows
@@ -63,15 +66,15 @@ test_df = pd.read_csv('data/pycoffee-random.csv')
 print(test_df.head(5)) # print only first 5 rows
 print('\n--------------------')
 print('Select column(s) by label\n')
-print(test_df['Příprava'].head(5))
-print(test_df[['Uživatel', 'Známka']].head(5))
+print(test_df['BrewingMethod'].head(5))
+print(test_df[['User', 'Rating']].head(5))
 print('\n--------------------')
 print('Select columns by data type\n')
 print(test_df.dtypes)
 print(test_df.select_dtypes(include='int64', exclude=None).head(5))
 print('\n--------------------')
 print('Select rows using boolean operators\n')
-print(test_df[test_df['Uživatel'] == 'Aestas'].head(5))
-print(test_df[(test_df['Uživatel'] == 'Cruduk') & 
-              (test_df['Pražírna'] == 'Laura Coffee')])
-print(test_df[test_df['Známka'] <= 2].head(5))
+print(test_df[test_df['User'] == 'Aestas'].head(5))
+print(test_df[(test_df['User'] == 'Cruduk') & 
+              (test_df['Roaster'] == 'Laura Coffee')])
+print(test_df[test_df['Rating'] <= 2].head(5))
